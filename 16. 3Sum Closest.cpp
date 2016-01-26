12219c945a1b9b144dbf6e5ff0132e6a141966e2
LIU Chengyang <464004340@qq.com>
2016-01-27 01:02:14 +0800

    注意条件。。。。

--- 16. 3Sum Closest.cpp
+++ 16. 3Sum Closest.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
+        int res = INT_MAX;
+        sort(nums.begin(),nums.end());
+        int size = nums.size();
+        if(size<3) return res;
+        res = nums[0] + nums[1] + nums[2];
+        for(int i = 0; i<size;i++){
+            int j = i+1, k = size-1;
+            while(j<k){
+                int tmp = nums[i] + nums[j]+nums[k];
+                if(tmp == target) return target;
+                else if(tmp < target){
+                    j++;
+                }
+                else {
+                    k--;
+                }
+                if(abs(target -tmp) < abs(target-res)){
+                    res = tmp;
+                }
+                
+            }
+            
+            
+        }
+        return res;
         
         
     }
