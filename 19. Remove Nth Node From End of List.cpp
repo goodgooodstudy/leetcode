44108e984b0a859dfe1d64e967919e4fed54b747
LIU Chengyang <464004340@qq.com>
2016-01-27 17:21:36 +0800

    首个节点有可能被删掉&& 且保证了倒数n个一定存在。可以去掉一些条件

--- 19. Remove Nth Node From End of List.cpp
+++ 19. Remove Nth Node From End of List.cpp
@@ -34,3 +34,39 @@ public:
         
     }
 };
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+//         ListNode* one = new ListNode(0);
+//         one ->next = head;
+//         ListNode*r = one, *l = one;
+
+//         while( n!=0){
+//             r = r->next;
+//             n--;
+//         }
+//         while(r->next!=NULL){
+//             r= r->next;
+//             l = l->next;
+//         }
+//         ListNode* del = l->next;
+//         l->next = del->next;
+//         delete(del);
+//         head= one->next;
+//         delete(one);
+//         return head;
+        
+        
+        
+        
+        
+//     }
+// };
