730fe52c23fdf470dc059b9d295cc9fcbeb3877e
LIU Chengyang <464004340@qq.com>
2016-01-30 13:57:00 +0800

    我的脑子应该想不出来。。。。好纠结啊，懒得不想申了

--- 22. Generate Parentheses.cpp
+++ 22. Generate Parentheses.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     vector<string> generateParenthesis(int n) {
+        vector<string> allComb;
+        string Comb;
+        generate(n,0,0,Comb,allComb);
+        return allComb;
         
+    }
+    void generate(int n, int left, int right, string s, vector<string>& all){
+        if(left== n && right==n){
+            all.push_back(s);
+            return;
+        }
+        if(left<n){
+            generate(n,left+1,right,s+'(',all);
+        }
+        if(right<left){
+            generate(n,left,right+1,s+')',all);
+        }
     }
 };
