87648832312c7c6834ba1474e90e325ec41ad875
LIU Chengyang <464004340@qq.com>
2016-02-03 16:58:56 +0800

    二分查找

--- 34. Search for a Range.cpp
+++ 34. Search for a Range.cpp
@@ -27,3 +27,34 @@ public:
         
     }
 };
+
+class Solution {
+private:
+    int binarySearchLow(vector<int>& nums, int begin, int end,int target){
+        if(begin > end) return begin;
+        int mid = begin + (-begin+end)/2;
+        if(nums[mid] < target) return binarySearchLow(nums, mid+1, end, target);
+        else return binarySearchLow(nums,begin, mid-1, target);
+    }
+    int binarySearchHigh(vector<int>& nums, int begin, int end, int target){
+        if(begin > end) return end;
+        int mid = begin+ (-begin+end)/2;
+        if(nums[mid] > target) return binarySearchHigh(nums, begin, mid-1, target);
+        else return binarySearchHigh(nums, mid+1, end, target);
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> res(2,-1);
+        if(nums.empty()) return res;
+        int high = binarySearchHigh(nums, 0, nums.size() -1,target);
+        int low = binarySearchLow(nums,  0, nums.size() - 1,target);
+        if(high >= low)
+        {
+            res[0] = low;
+            res[1] = high;
+            return res;
+        }
+        return res;
+    }
+        
+};
