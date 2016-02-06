d8281c881add2847df557823b58f761be711ff7d
LIU Chengyang <464004340@qq.com>
2016-02-06 20:26:20 +0800

    dfs&&知道map和unordered_map

--- 332. Reconstruct Itinerary.cpp
+++ 332. Reconstruct Itinerary.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     vector<string> findItinerary(vector<pair<string, string>> tickets) {
+        unordered_map<string, map<string, int>> map;
+        vector<string> route;
+        int size = tickets.size()+1;
+        route.push_back("JFK");
+        for (const auto& t: tickets){
+            map[t.first][t.second]++;
+        }
+        dfs("JFK",map,route,size);
+        return route;
+        
+    }
+    bool dfs(string cur, unordered_map<string, map<string, int>>& map, vector<string>& route, int size ){
+        if(route.size() == size) return true;
+        if(route.size() > size) return false;
+        
+        for(auto ticket = map[cur].begin(); ticket!=map[cur].end();ticket++){
+            if(ticket->second!=0){
+                ticket->second --;
+                route.push_back(ticket->first);
+                if(dfs(ticket->first, map, route, size))return true;
+                route.pop_back();
+                ticket->second++;
+            }
+        }
+        return false;
         
     }
 };
