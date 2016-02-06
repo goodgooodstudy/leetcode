edeaf55a0ac67b55b90b132aed2785ae66dbab0d
LIU Chengyang <464004340@qq.com>
2016-02-06 22:57:57 +0800

    我的脑子啊。。。

--- 331. Verify Preorder Serialization of a Binary Tree.cpp
+++ 331. Verify Preorder Serialization of a Binary Tree.cpp
@@ -16,19 +16,30 @@ public:
                     // stack.pop_back();
                     stack.erase(stack.end()-3,stack.end());
                     stack.push_back("#");
-
-
                 }
                 else break;
-
             }
-        
-            
         }
         if (stack.size() == 1 && stack[0] =="#") {
             return true;
         }
         else return false;
         
+    }
+};
+
+// hard to come up with 
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        string node;
+        stringstream ss(preorder);
+        int diff = 1;
+        while (getline(ss, node, ',')) {
+            if(--diff <0) return false;
+            if(node != "#") diff+=2;
+            
+        }
+        return diff == 0;
     }
 };
