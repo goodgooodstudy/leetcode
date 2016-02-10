efa900fb07c7b5a8a73dc8b08c8683fdef81b202
LIU Chengyang <464004340@qq.com>
2016-02-10 14:00:45 +0800

    Update 330. Patching Array.cpp

--- 330. Patching Array.cpp
+++ 330. Patching Array.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     int minPatches(vector<int>& nums, int n) {
-        
+        sort(nums.begin(),nums.end());
+        long miss = 1; // n = 2147483647 可能会overflow
+        int cnt = 0;
+        int i = 0;
+        while(miss <= n){
+            if(i < nums.size()&&nums[i] <= miss){
+                miss  = miss+nums[i];
+                i++;
+            }
+            else{
+                cnt++;
+                miss = miss+miss;
+            }
+        }
+        return cnt;
         
     }
 };
