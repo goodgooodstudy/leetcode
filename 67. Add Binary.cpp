bab7a74cfe50d2e1b6e8dcb23c65a0758721bb69
LIU Chengyang <464004340@qq.com>
2016-02-21 22:15:25 +0800

    Create 67. Add Binary.cpp

---
+++ 67. Add Binary.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string c;
+        reverse(a.begin(),a.end());
+        reverse(b.begin(),b.end());
+        int t1, t2, add;
+        add = 0;
+        for(int i = 0; i < a.size()||i < b.size(); i++){
+            if(i >= a.size()) t1 = 0;
+            else t1 = a[i] -'0';
+            if(i >= b.size()) t2 = 0;
+            else t2 = b[i] - '0';
+            
+            c.push_back((t1+t2+add)%2+'0');
+            add = (t1+t2+add)/2;
+        }
+        if(add == 1) c.push_back('1');
+        reverse(c.begin(),c.end());
+        return c;
+    }
+};
