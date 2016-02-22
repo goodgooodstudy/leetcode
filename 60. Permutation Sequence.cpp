1cfd5f5475fb9b9b90ee6e58d07f42f5ee2b626a
LIU Chengyang <464004340@qq.com>
2016-02-22 22:14:03 +0800

    love coding && hate reading paper....

--- 60. Permutation Sequence.cpp
+++ 60. Permutation Sequence.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        
+        vector<int> sorted_list;
+        string result;
+        int permutation = 1;
+        for(int i = 1; i <= n; i++){
+            sorted_list.push_back(i);
+            permutation = permutation*i;
+        }
+        k = k-1;
+        for(int i = 0; i < n;i++){
+            permutation = permutation/(n-i);
+            int index = k/permutation;
+            result.push_back(sorted_list[index]+'0');
+            sorted_list.erase(sorted_list.begin()+index);
+            k = k%permutation;
+        }
+        // reverse(result.begin(),result.end());
+        return result;
         
     }
 };
