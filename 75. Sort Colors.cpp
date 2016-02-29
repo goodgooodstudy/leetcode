0937499a5e6591dfd96c362f148584a83819c2a7
LIU Chengyang <464004340@qq.com>
2016-02-29 18:20:50 +0800

    毕设要完蛋了。。。。。

---
+++ 75. Sort Colors.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        // sort(nums.begin(),nums.end());
+        int count0 = 0, count1 = 0, count2 = 0;
+        for(auto num: nums){
+            if(num == 0) count0++;
+            if(num == 1) count1++;
+            if(num == 2) count2++;
+        }
+        for(int i = 0; i < count0; i++){
+            nums[i] = 0;
+        }
+        for(int i = count0; i < count0+count1; i++){
+            nums[i] =1;
+        }
+        for(int i = count0+count1; i < count0+count1+count2; i++){
+            nums[i] = 2;
+        }
+        
+    }
+};
