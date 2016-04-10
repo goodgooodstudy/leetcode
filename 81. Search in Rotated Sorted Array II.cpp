f5069edcfff9fb1b6cc49477c89fc641d812b1c5
LIU Chengyang <464004340@qq.com>
2016-04-10 22:12:41 +0800

    Create 81. Search in Rotated Sorted Array II.cpp

---
+++ 81. Search in Rotated Sorted Array II.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int first = 0;
+        int last = nums.size()-1;
+        while(first <= last){
+            int mid = (first+last)/2;
+            if(nums[mid] == target) return true;
+            if(nums[first] < nums[mid]){
+                if(nums[first] <= target && target < nums[mid])
+                    last = mid -1;
+                else
+                    first = mid+1;
+            }
+            else if(nums[first] > nums[mid]){
+                if(nums[mid] < target && target <= nums[last])
+                    first = mid+1;
+                else
+                    last = mid-1;
+            }
+            else{
+                first++;
+            }
+        }
+        return false;
+        
+    }
+};
