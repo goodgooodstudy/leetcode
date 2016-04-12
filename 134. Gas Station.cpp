29727e97229c9a3d8b98a79647920ebd09b7c9a2
LIU Chengyang <464004340@qq.com>
2016-04-13 01:15:31 +0800

    Create 134. Gas Station.cpp

---
+++ 134. Gas Station.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int sum = 0;
+        int total = 0;
+        int index = -1;
+        for(int i = 0; i<gas.size(); i++){
+            sum+= gas[i] - cost[i];
+            total += gas[i] - cost[i];
+            if(sum < 0){
+                sum = 0;
+                index = i;
+            }
+        }
+        if(total >= 0) return index+1 < gas.size()?index+1:0;
+        else return -1;
+    }
+};
