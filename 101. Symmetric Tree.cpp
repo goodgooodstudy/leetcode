8747ed1fe92820288a75ab17a332a8382085b87b
LIU Chengyang <464004340@qq.com>
2016-06-22 17:09:14 +0800

    Update 101. Symmetric Tree.cpp

--- 101. Symmetric Tree.cpp
+++ 101. Symmetric Tree.cpp
@@ -19,3 +19,31 @@ public:
         return left->val == right->val && isSymmetric(left->left,right->right) && isSymmetric(left->right,right->left);
     }
 };
+
+
+
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+            if(root == NULL) return true;
+            stack<TreeNode *> s;
+            s.push(root->left);
+            s.push(root->right);
+            while(!s.empty()){
+                auto p = s.top();
+                s.pop();
+                auto q = s.top();
+                s.pop();
+                
+                if(!p && !q) continue;
+                if(!p || !q) return false;
+                if(p->val != q->val) return false;
+                s.push(p->left);
+                s.push(q->right);
+                s.push(p->right);
+                s.push(q->left);
+            }
+            return true;
+    }
+   
+};
