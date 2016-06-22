bdcb1d7b47ade9ab22bab752ecc2369313634cb1
LIU Chengyang <464004340@qq.com>
2016-06-22 17:16:28 +0800

    Update 100. Same Tree.cpp

--- 100. Same Tree.cpp
+++ 100. Same Tree.cpp
@@ -15,3 +15,26 @@ public:
         return p->val == q->val && isSameTree(p->left,q->left) && isSameTree(p->right, q->right);
     }
 };
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        stack<TreeNode*> s;
+        s.push(p);
+        s.push(q);
+        while(!s.empty()){
+            auto p = s.top();
+            s.pop();
+            auto q = s.top();
+            s.pop();
+            if(!p && !q) continue;
+            if(!p || !q) return false;
+            if(p->val != q->val) return false;
+            
+            s.push(p->left);
+            s.push(q->left);
+            s.push(p->right);
+            s.push(q->right);
+        }
+        return true;
+    }
+};
