36e2742489c33f7f2890c7fb235baa1ccf992d30
LIU Chengyang <464004340@qq.com>
2016-06-22 17:51:12 +0800

    Update 104. Maximum Depth of Binary Tree.cpp

--- 104. Maximum Depth of Binary Tree.cpp
+++ 104. Maximum Depth of Binary Tree.cpp
@@ -29,3 +29,11 @@ public:
         
     }
 };
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        return root ==NULL?0:max(maxDepth(root->left),maxDepth(root->right))+1;
+        
+    }
+};
