d85b1709d58baf0cfb33bd2bd43ba6cf477a0e0e
LIU Chengyang <464004340@qq.com>
2016-07-21 14:22:14 +0800

    Create 108. Convert Sorted Array to Binary Search Tree.cpp

---
+++ 108. Convert Sorted Array to Binary Search Tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return BuildBST(nums,0,nums.size()-1);
+        
+    }
+    TreeNode* BuildBST(vector<int>& nums, int start, int end){
+        if(start > end) return NULL;
+        if(start == end) return new TreeNode(nums[start]);
+        int mid = (start+end)/2;
+        TreeNode* root = new TreeNode(nums[mid]);
+        root->left = BuildBST(nums,start,mid-1);
+        root->right = BuildBST(nums,mid+1,end);
+        return root;
+    }
+};
