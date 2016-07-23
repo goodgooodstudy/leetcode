1dd40ad06139656bebfa23c22baf8b8709658008
LIU Chengyang <464004340@qq.com>
2016-07-23 19:42:02 +0800

    Update 124. Binary Tree Maximum Path Sum.cpp

--- 124. Binary Tree Maximum Path Sum.cpp
+++ 124. Binary Tree Maximum Path Sum.cpp
@@ -28,3 +28,26 @@ public:
             std::max(node->val+left, node->val+right));  
         }  
 };
+
+
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        max_sum=INT_MIN;  
+        dfs(root);
+        return max_sum;  
+    }  
+private:
+    int max_sum;
+    int dfs(TreeNode* root){
+        if(root == NULL) return 0;
+        int l = dfs(root->left);
+        int r = dfs(root->right);
+        int sum = root->val;
+        if(l>0) sum+=l;
+        if(r>0) sum+=r;
+        max_sum = max(sum, max_sum);
+        return max(r,l)>0?root->val+max(r,l):root->val;
+        
+    }
+};
