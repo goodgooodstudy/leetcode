9ae067da2d7a616ca75b6f9de59b9ecdf17986be
LIU Chengyang <464004340@qq.com>
2016-07-27 16:07:24 +0800

    Update 131. Palindrome Partitioning.cpp

--- 131. Palindrome Partitioning.cpp
+++ 131. Palindrome Partitioning.cpp
@@ -28,3 +28,40 @@ public:
         return true;
     }
 };
+
+
+
+
+dp judge ispal
+class Solution {
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> rst;
+        vector<string> cur;
+        int n = s.size();
+        vector<vector<bool>> isPal(n, vector<bool>(n,false));
+
+        for(int i = n-1; i >= 0; i--){
+            for(int j = i; j < n; j++){
+                isPal[i][j] = (s[i] == s[j]) &&(j-i<=2||isPal[i+1][j-1]); 
+
+            }
+        }
+        DFS(s,0,isPal,rst,cur);
+        return rst;
+    }
+    void DFS(string& s, int start, vector<vector<bool>>& isPal, vector<vector<string>>& rst, vector<string>& cur ){
+        if(start == s.size()){
+            rst.push_back(cur);
+            return;
+        }
+        for(int i = start; i < s.size();i++){
+            if(isPal[start][i]){
+                cur.push_back(s.substr(start,i-start+1));
+                DFS(s,i+1,isPal,rst,cur);
+                cur.pop_back();
+            }
+        }
+    }
+
+};
