c3778e6d8e91f0cdca4f67020414a74a14a0d827
LIU Chengyang <464004340@qq.com>
2016-08-01 14:09:23 +0800

    Update 378. Kth Smallest Element in a Sorted Matrix.cpp

--- 378. Kth Smallest Element in a Sorted Matrix.cpp
+++ 378. Kth Smallest Element in a Sorted Matrix.cpp
@@ -23,3 +23,33 @@ public:
         bool operator()(int a, int b){return a < b;}
     };
 };
+
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        int n = matrix.size();
+        if(min(n,k) == 0) return 0;
+        auto cmp = [&matrix](pair<int, int>a, pair<int, int>b){return matrix[a.first][a.second]>matrix[b.first][b.second];};
+       priority_queue<pair<int, int>, vector<pair<int,int>>,decltype(cmp)> heap(cmp);
+       heap.emplace(0,0);
+       int ans;
+       while(k-- > 0 && heap.size()){
+           auto pair = heap.top();
+           ans = matrix[pair.first][pair.second];
+
+           heap.pop();
+           if(pair.second + 1 < n ){
+               heap.emplace(pair.first,pair.second+1);
+           }
+           if(pair.second == 0 && pair.first+1 < n){
+               heap.emplace(pair.first+1, pair.second);
+           }
+           
+       }
+
+       return ans;
+        
+    }
+
+};
