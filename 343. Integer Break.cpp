ce8492a7d2fe58339ea9f015c2d6aed222323a65
LIU Chengyang <464004340@qq.com>
2016-08-02 15:24:09 +0800

    Update 343. Integer Break.cpp

--- 343. Integer Break.cpp
+++ 343. Integer Break.cpp
@@ -11,3 +11,32 @@ public:
         
     }
 };
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        vector<int>dp(n+1,0);
+        if(n<=3) return n-1;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for(int i = 4; i<=n; i++){
+            dp[i] = max(dp[i-2]*2,dp[i-3]*3);
+        }
+        
+        return dp[n];
+        
+    }
+};
+
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n<=3) return n-1;
+        int res = n%3;
+        if(res == 0) return pow(3,n/3);
+        else if(res == 1) return 4*pow(3,(n-4)/3);
+        else  return 2* pow(3,(n-2)/3);
+
+    }
+};
