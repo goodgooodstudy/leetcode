f68f9114e27e219451b0aab6d267dac7efbf3ac8
LIU Chengyang <464004340@qq.com>
2016-08-03 21:26:06 +0800

    Update 123. Best Time to Buy and Sell Stock III.cpp

--- 123. Best Time to Buy and Sell Stock III.cpp
+++ 123. Best Time to Buy and Sell Stock III.cpp
@@ -40,3 +40,29 @@ f2[i]表示 -- 截止到i下标为止，右边所做交易能够达到最大prof
         return sum;
     }
 };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n<2) return 0;
+        int minPrice=prices[0];
+        int maxProfit = 0;
+        vector<int> leftProfit(n,0);
+        for(int i = 1; i<n;i++){
+            minPrice=min(minPrice,prices[i]);
+            maxProfit=max(maxProfit,prices[i]-minPrice);
+            leftProfit[i]=maxProfit;
+        }
+        int maxPrice = prices[n-1];
+        maxProfit = 0;
+        int ret = 0;
+        for(int i = n-2; i>=0;i--){
+            maxPrice = max(maxPrice,prices[i]);
+            maxProfit=max(maxProfit,maxPrice-prices[i]);
+            ret=max(ret,leftProfit[i]+maxProfit);
+        }
+        return ret;
+        
+    }
+};
