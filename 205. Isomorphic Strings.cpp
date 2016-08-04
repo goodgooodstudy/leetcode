d4d963d3f9d6a45aaa239148f51b7ce7a0af52c1
LIU Chengyang <464004340@qq.com>
2016-08-04 22:16:44 +0800

    Create 205. Isomorphic Strings.cpp

---
+++ 205. Isomorphic Strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> maps1;
+        unordered_map<char, char> maps2;
+
+        for(int i = 0; i<s.size(); i++){
+            if(maps1.count(s[i])||maps2.count(t[i])){
+                if(t[i]!=maps1[s[i]]||s[i]!=maps2[t[i]]) return false;
+            }
+            else{
+                cout << "ASD"<<endl;
+                maps1[s[i]] = t[i];
+                maps2[t[i]] = s[i];
+            }
+        }
+        return true;
+        
+    }
+};
