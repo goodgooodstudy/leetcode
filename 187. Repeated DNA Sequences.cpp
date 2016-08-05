84176695a536226d5fba0f35dd74f2cee4179fa8
LIU Chengyang <464004340@qq.com>
2016-08-05 15:39:55 +0800

    Create 187. Repeated DNA Sequences.cpp

---
+++ 187. Repeated DNA Sequences.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_map<string,int> maps;
+        vector<string> res;
+        for(int i = 10; i <= s.size(); i++){
+            string cur = s.substr(i-10,10);
+            if(maps.count(cur)&&maps[cur] ==1){
+                res.push_back(cur);
+                maps[cur]++;
+            }
+            else{
+                maps[cur] ++;
+            }
+        }
+        return res;
+        
+    }
+};
+
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_map<int,int> maps;
+        vector<string> res;
+        for(int i = 10; i <= s.size(); i++){
+            string cur = s.substr(i-10,10);
+            int code = encode(cur);
+            if(maps.count(code)&&maps[code] ==1){
+                res.push_back(cur);
+                maps[code]++;
+            }
+            else{
+                maps[code] ++;
+            }
+        }
+        return res;
+        
+    }
+    int encode(string s){
+        int res=0;
+        for(int i = 0; i < 10; i++){
+            res = res<<2;
+            switch (s[i]){
+                case 'A':res+=0;break;
+                case 'C':res+=1;break;
+                case 'G':res+=2;break;
+                case 'T':res+=3;break;
+            }
+        }
+        return res;
+    }
+};
