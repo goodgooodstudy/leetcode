78fb525f112d10a6f34f887d15839e997477edda
LIU Chengyang <464004340@qq.com>
2016-08-05 21:14:26 +0800

    Create 219. Contains Duplicate II.cpp

---
+++ 219. Contains Duplicate II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> maps;
+        for(int i = 0; i < nums.size(); i++){
+            if(maps.count(nums[i])){
+                if(i-maps[nums[i]]<=k) return true;
+                maps[nums[i]] = i;
+            }
+            else{
+                maps[nums[i]] = i;
+            }
+        }
+        return false;
+        
+    }
+};
