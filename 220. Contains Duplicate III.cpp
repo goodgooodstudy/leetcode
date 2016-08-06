aa3fc403da50d57b94fd3b4672d9b0dace6438bf
LIU Chengyang <464004340@qq.com>
2016-08-06 19:39:29 +0800

    Create 220. Contains Duplicate III.cpp

---
+++ 220. Contains Duplicate III.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+//  bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+//     set<int> window; // set is ordered automatically 
+//     for (int i = 0; i < nums.size(); i++) {
+//         if (i > k) window.erase(nums[i-k-1]); // keep the set contains nums i j at most k
+//         // |x - nums[i]| <= t  ==> -t <= x - nums[i] <= t;
+//         auto pos = window.lower_bound(nums[i] - t); // x-nums[i] >= -t ==> x >= nums[i]-t 
+//         // x - nums[i] <= t ==> |x - nums[i]| <= t    
+//         if (pos != window.end() && *pos - nums[i] <= t) return true;
+//         window.insert(nums[i]);
+//     }
+//     return false;
+// }
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        set<int> windows;
+        for(int i = 0; i < nums.size(); i++){
+            if(i > k) windows.erase(nums[i-k-1]);
+            long long low = 0;
+            long long up = 0;
+            low = low + nums[i]-t;
+            up = up + nums[i] + t;
+            // cout << up<<endl;
+            auto it = windows.lower_bound( low);
+            // if(it != windows.end() && *it - nums[i]<= t)return true; //*it <= nums[i]+t overflow
+            if(it != windows.end() && *it <= up)return true; //*it <= nums[i]+t overflow
+
+            windows.insert(nums[i]);
+        }
+        return false;
+        
+    }
+};
