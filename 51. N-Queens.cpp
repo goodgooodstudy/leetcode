1d38ce17e9fe32c002105939727dd9d275f27dfe
LIU Chengyang <464004340@qq.com>
2016-08-10 17:10:35 +0800

    Create 51. N-Queens.cpp

---
+++ 51. N-Queens.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    int n;
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        this->n = n;
+        vector<vector<string>> res;
+        vector<string> ans (n,string(n,'.'));
+        solNQueensHelper(res,ans,0);
+        return res;
+        
+    }
+    void solNQueensHelper(vector<vector<string>>& res,vector<string>& ans,int row){
+        if(row == n) res.push_back(ans);
+        for(int i = 0; i < n; i++){
+            if(isValid(ans,row,i)){
+                ans[row][i] = 'Q';
+                solNQueensHelper(res,ans,row+1);
+                ans[row][i] = '.';
+            }
+        }
+        
+    }
+    bool isValid(vector<string>& ans,int row,int col){
+        for(int i = 0; i!=row; i++){
+            if(ans[i][col] == 'Q')return false;
+        }
+        //check 135 degrees
+        for(int r = row-1, c = col-1; r>=0 &&c >=0; r--,c--){
+            if(ans[r][c] == 'Q') return false;
+        }
+        for(int r = row-1, c = col+1; r>=0 && c < n; r--,c++){
+            if(ans[r][c] == 'Q') return false;
+        }
+        return true;
+        
+    }
+};
