6174daef373321f1416660e3ce61fecc5314d9fa
LIU Chengyang <464004340@qq.com>
2016-08-12 17:59:49 +0800

    Create 135. Candy.cpp

---
+++ 135. Candy.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int sum = 0;
+        vector<int> candy(ratings.size(),1);
+        for(int i = 1; i < ratings.size();i++){
+            if(ratings[i] > ratings[i-1]){
+                candy[i] = candy[i-1]+1;
+            }
+
+        }
+
+        
+        for(int i = ratings.size()-2; i >=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                candy[i] = max( candy[i],  candy[i+1]+1); 
+            }
+        }
+        for(int i = 0; i < ratings.size(); i++){
+            sum+=candy[i];
+        } 
+        return sum;
+        
+    }
+};
