a705a2501e7b2fcf6acf6c97a0e9bf6c1cb4aa02
LIU Chengyang <464004340@qq.com>
2016-09-06 11:23:23 -0400

    Create 305. Number of Islands II.cpp

---
+++ lock/305. Number of Islands II.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> numIslands2(int m, int n, vector<pair<int, int>>& positions) {
+        vector<int> roots(m*n,-1);
+        vector<int> res;
+        vector<pair<int,int>> dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+        int island = 0;
+        for(auto p : positions){
+            int x = p.first, y = p.second, p_idx = n*x + y;
+            roots[p_idx] = p_idx;
+            ++island;
+            
+            for(auto dir: dirs){
+                int row = x+dir.first, col = y+dir.second;
+                int new_idx = n*row + col;
+                if(row>=0 && row<m && col>=0 && col < n && roots[new_idx]!=-1){
+                    int p_root = find(p_idx, roots), n_root = find(new_idx, roots);
+                    if(p_root != n_root){
+                        roots[p_root] = n_root;
+                        --island;
+                    }
+                }
+            }
+            res.push_back(island);
+        }
+        return res;
+    }
+    
+    int find(int idx, vector<int>& roots){
+        while(roots[idx]!=idx){
+            roots[idx] = roots[roots[idx]];
+            idx = roots[idx];
+        }
+        return idx;
+    }
+};
