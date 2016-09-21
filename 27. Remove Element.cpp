9d2faaa678614f4d25dd054312e67ffc7550326c
LIU Chengyang <464004340@qq.com>
2016-09-20 23:10:56 -0400

    Update 27. Remove Element.cpp

--- 27. Remove Element.cpp
+++ 27. Remove Element.cpp
@@ -6,3 +6,31 @@ public:
         return nums.size();
     }
 };
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        // int cnt = 0;
+        int n = nums.size();
+        int i = 0;
+        while( i < n){
+            if(nums[i] == val){
+                nums[i]= nums[n-1];
+                n--;
+            }
+            else{
+                i++;
+            }
+        }
+        return n;
+        // for(int i = 0; i < nums.size()-first; i++){
+        //     if(nums[i] == val){
+        //         nums[i] = nums[nums.size()-1-first];
+        //         first++;
+        //         i--;
+        //     }
+        // }
+        // return nums.size()-first;
+        
+    }
+};
