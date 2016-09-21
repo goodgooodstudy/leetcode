db8e4648625e0115f3e09dec8f90803fc5c29193
LIU Chengyang <464004340@qq.com>
2016-09-21 11:19:44 -0400

    Update 33. Search in Rotated Sorted Array.cpp

--- 33. Search in Rotated Sorted Array.cpp
+++ 33. Search in Rotated Sorted Array.cpp
@@ -28,3 +28,34 @@ public:
     }
     
 };
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0,  r = nums.size()-1;
+        while(l <= r){
+            int mid = l + (r-l)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[l] < nums[mid]){
+                if(target < nums[l] || target > nums[mid]){
+                    l = mid+1;
+                }
+                else{
+                    r = mid-1;
+                }
+                
+            }
+            else{
+                if(target < nums[mid+1] || target > nums[r]){
+                    r = mid -1;
+                }
+                else{
+                    l = mid+1;
+                }
+            }
+
+        }
+        return -1;
+    }
+    
+};
