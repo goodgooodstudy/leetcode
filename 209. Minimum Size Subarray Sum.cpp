7d9d779e126f4aa79552a79a2686d46d847aaffc
LIU Chengyang <464004340@qq.com>
2016-09-22 00:41:13 -0400

    Update 209. Minimum Size Subarray Sum.cpp

--- 209. Minimum Size Subarray Sum.cpp
+++ 209. Minimum Size Subarray Sum.cpp
@@ -33,3 +33,49 @@ public:
         return minLen == INT_MAX?0:minLen;
     }
 };
+
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        vector<int> sums(nums.size()+1,0);
+        calculate(sums, nums);
+        int mx = INT_MAX;
+        for(int i = 1; i <= nums.size(); i++){
+            if(sums[i] >= s){
+                 int p = upper_bound(sums,0,i, sums[i]-s);
+                if(p!=-1) mx = min(mx, i-p+1);
+            }
+           
+            
+        }
+        return mx == INT_MAX?0:mx;
+    }
+    void calculate(vector<int>& sum, vector<int>& nums){
+        int s = nums[0];
+        for(int i = 1; i <= nums.size(); i++){
+            sum[i] = nums[i-1] + sum[i-1];
+        }
+    }
+    //  int upper_bound(vector<int>& sums, int left, int right, int target) {
+    //     int l = left, r = right;
+    //     while (l < r) {
+    //         int m = l + ((r - l) >> 1);
+    //         if (sums[m] <= target) l = m + 1;
+    //         else r = m;
+    //     }
+    //     return sums[r] > target ? r : -1;
+    // }
+    int upper_bound(vector<int>& sums, int l, int r, int target){
+        while(l < r){
+            int mid = l + (r-l)/2;
+            if(target >= sums[mid]){
+                l = mid+1;
+            }
+            else r = mid;
+            
+        }
+        if(sums[r] > target) return r;
+        return -1;
+    }
+};
