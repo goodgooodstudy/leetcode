f710f0cce66ed9c918f492418725c7b4c73d2224
LIU Chengyang <464004340@qq.com>
2016-09-21 22:55:42 -0400

    Update 216. Combination Sum III.cpp

--- 216. Combination Sum III.cpp
+++ 216. Combination Sum III.cpp
@@ -17,3 +17,27 @@ public:
         }
     }
 };
+
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<vector<int>> rst;
+        vector<int> path;
+        backtracking(rst, path, k,n,1);
+        return rst;
+    }
+    void backtracking(vector<vector<int>>& rst, vector<int>& path, int k, int target, int start){
+        if(k==0 && target==0 ){
+            rst.push_back(path);
+            return;
+        }
+        if(k==0 || target <= 0) return;
+        for(int i = start; i <= 9; i++){
+            path.push_back(i);
+            backtracking(rst, path, k-1, target-i,i+1);
+            path.pop_back();
+        }
+        
+    }
+};
