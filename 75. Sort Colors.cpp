cc82b8dbd1ca6a14c0d1e7ee3456cb774eef2d11
LIU Chengyang <464004340@qq.com>
2016-09-23 19:26:32 -0400

    Update 75. Sort Colors.cpp

--- 75. Sort Colors.cpp
+++ 75. Sort Colors.cpp
@@ -20,3 +20,17 @@ public:
         
     }
 };
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero = 0, second = nums.size()-1;
+        for(int i = 0; i <= second; ){
+           if(nums[i] == 0) swap(nums[i++], nums[zero++]);
+           else if(nums[i] == 2) swap(nums[i], nums[second--]);
+           else i++;
+           
+
+        }
+        
+    }
+};
