65a67390f97787a40576eab3cf3a91e13fcd9891
LIU Chengyang <464004340@qq.com>
2016-09-27 11:56:33 -0400

    Update 38. Count and Say.cpp

--- 38. Count and Say.cpp
+++ 38. Count and Say.cpp
@@ -25,3 +25,26 @@ public:
         
     }
 };
+class Solution {
+public:
+    string countAndSay(int n){
+        if(n == 0) return "";
+        string res = "1";
+        while(--n > 0){
+            string tmp;
+            for(int i = 0; i <res.size(); i++){
+                int count = 1;
+                while(i+1 < res.size() && res[i] == res[i+1]){
+                    count ++;
+                    i++;
+                }
+                tmp += to_string(count) + res[i];
+            }
+            res = tmp;
+        }
+        return res;
+
+    
+    }
+  
+};
