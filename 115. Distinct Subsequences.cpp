54424084adbc4584a2d7168e38dd0c9883e5c50e
LIU Chengyang <464004340@qq.com>
2016-09-28 00:22:31 -0400

    Update

--- 115. Distinct Subsequences.cpp
+++ 115. Distinct Subsequences.cpp
@@ -19,3 +19,32 @@ public:
         
     }
 };
+
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.size(), n = t.size();
+        if(m==0 && n==0) return 1;
+        if(m==0||n==0) return 0;
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        if(s[0] == t[0])dp[0][0]=1;
+        for(int i = 1; i < m; i++){
+            if(s[i] == t[0]) dp[i][0] = dp[i-1][0]+1;
+            else dp[i][0] = dp[i-1][0];
+        }
+        
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                if(s[i] == t[j]){
+                    dp[i][j] = dp[i-1][j-1]+dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[m-1][n-1];
+        
+    }
+};
