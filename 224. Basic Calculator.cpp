de38cc5b32eec538fd611e0273ad5fa3b7ed8976
LIU Chengyang <464004340@qq.com>
2016-09-29 09:27:03 -0400

    Update 224. Basic Calculator.cpp

--- 224. Basic Calculator.cpp
+++ 224. Basic Calculator.cpp
@@ -34,3 +34,45 @@ public:
     }
     
 };
+
+class Solution {
+public:
+    int calculate(string s) {
+        int total = 0;
+        int sign = 1;
+        int i = 0;
+        for(int i = 0;  i < s.size(); i++){
+            if(s[i] == ' '){
+                continue;
+            }
+            else if(s[i] == '('){
+                int right = findright(s, i);
+                total = total + sign*calculate(s.substr(i+1,right-i-1));
+                i = right;
+            }
+            else if(s[i] == '+' || s[i] == '-'){
+                sign = s[i] == '+' ? 1 : -1;
+            }
+            else{
+                int n = s[i]-'0';
+                while(i+1 < s.size() && isalnum(s[i+1])){
+                    n = n*10 + s[i+1]-'0';
+                    i++;
+                }
+                total = total + sign*n;
+            }
+            
+        }
+        
+        return total;
+    }
+    int findright(string s, int pos){
+        int left = 0;
+        for(int i = pos; i < s.size(); i++){
+            if(s[i] == '(') left++;
+            if(s[i] == ')') left--;
+            if(left == 0) return i;
+        }
+        return s.size()-1;
+    }
+};
