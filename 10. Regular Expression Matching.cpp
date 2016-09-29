c2707771dda39ecb300ebfeb863246a695367ad8
LIU Chengyang <464004340@qq.com>
2016-09-29 19:48:25 -0400

    Update 10. Regular Expression Matching.cpp

--- 10. Regular Expression Matching.cpp
+++ 10. Regular Expression Matching.cpp
@@ -31,3 +31,29 @@ public:
         return dp[m][n];
     }
 };
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int n1 = s.size(), n2 = p.size();
+        bool dp[n1+1][n2+1];
+        memset(dp, false, (n1+1)*(n2+1)*sizeof(bool));
+        dp[0][0] = true;
+        for(int i = 0; i <= n1; i++){
+            for(int j = 1; j <= n2; j++){
+                if(i>0 && s[i-1] == p[j-1]){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(i>0 && p[j-1] == '.'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(j > 1 && p[j-1] == '*'){
+                    dp[i][j] = dp[i][j-2] || dp[i][j-1];
+                    if (i>0 && (s[i-1] == p[j-2] || p[j-2] == '.')&&dp[i-1][j]) dp[i][j]= true;
+                }
+            }
+        }
+        return dp[n1][n2];
+        
+    }
+};
