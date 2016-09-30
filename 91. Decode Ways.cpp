0c46641d8e5c6ad0d590dec34c0b6eb48970656f
LIU Chengyang <464004340@qq.com>
2016-09-30 15:35:22 -0400

    Update 91. Decode Ways.cpp

--- 91. Decode Ways.cpp
+++ 91. Decode Ways.cpp
@@ -17,3 +17,30 @@ public:
         return r1;
     }
 };
+
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> len(n, 0);
+        if(n==0) return 0;
+        if(s[0]<'1' || s[0] > '9') return 0;
+        if(s[0] >='1' && s[0] <='9')len[0] = 1;
+        if(n == 1) return len[0];
+        if(s[1] >='1' && s[1] <='9')len[1] = 1;
+        if(isValid(s[0], s[1])) len[1] = len[1] + 1;
+        
+        for(int i = 2; i < n; i++){
+            if(s[i] >='1' && s[i] <='9')len[i] = len[i-1];
+            if(isValid(s[i-1], s[i])) len[i] += len[i-2];
+        }
+        return len[n-1];
+        
+        
+    }
+    bool isValid(char n1, char n2){
+        if(n1=='1' || n1 == '2' && n2 <='6') return true;
+        else return false;
+    }
+};
