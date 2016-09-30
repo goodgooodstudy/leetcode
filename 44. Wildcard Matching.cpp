6697872a2149c11b5feb774ec903fa04612d8d56
LIU Chengyang <464004340@qq.com>
2016-09-30 14:18:40 -0400

    Update 44. Wildcard Matching.cpp

--- 44. Wildcard Matching.cpp
+++ 44. Wildcard Matching.cpp
@@ -27,3 +27,35 @@ public:
         
     }
 };
+
+class Solution {
+private:
+    
+public:
+    bool isMatch(string s, string p) {
+        int n1 = s.size(), n2 = p.size();
+        cout << n1 << " "<<n2<<endl;
+        vector<bool> dp(n2+1,false);
+
+        dp[0] = true;
+        for(int j = 1; j <= n2; j++){
+            if(p[j-1] == '*') dp[j] = dp[j-1];
+        }
+        for(int i = 1; i <= n1; i++){
+            vector<bool> ori = dp;
+            dp[0] = false;
+            for(int j = 1; j <= n2; j++){
+                if(s[i-1] == p[j-1] || p[j-1] == '?') {
+                    dp[j] = ori[j-1];
+                }
+                else if(p[j-1] == '*'){
+                    dp[j] = dp[j-1] || ori[j];
+                }
+                else dp[j] = false;
+            }
+            
+        }
+        return dp[n2];
+
+    }
+};
