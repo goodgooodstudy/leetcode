d64f711262ff25c3bca7d88304ba012e1710841a
LIU Chengyang <464004340@qq.com>
2016-10-02 15:53:07 -0400

    Update 139. Word Break.cpp

--- 139. Word Break.cpp
+++ 139. Word Break.cpp
@@ -16,3 +16,40 @@ public:
         
     }
 };
+
+
+class Solution {
+private:
+    set<int> idx;
+public:
+    bool wordBreak(string s, unordered_set<string>& wordDict) {
+        
+        // vector<bool> mark(s.size()+1, false); //len
+        // mark[0] = true;
+        // int i = 0; 
+        // while(i <= s.size()){
+        //     for(auto d : wordDict){
+        //         if(s.substr(i,d.size()) == d){
+        //             mark[i+d.size()] = true;
+        //         }
+        //     }
+        //     i++;
+        //     while(i<=s.size()&&mark[i] == false) {
+        //         i++;
+        //     }
+        // }
+        // return mark[s.size()];
+        return helper(s, 0, wordDict);
+        
+    }
+    bool helper(string& s, int pos, unordered_set<string>& wordDict){
+        if(pos == s.size()) return true;
+        if(idx.count(pos)) return false;
+        for(int i = 1; i <= s.size()-pos; i++){
+            string tmp = s.substr(pos, i);
+            if(wordDict.count(tmp) && helper(s, pos+i, wordDict)) return true;
+        }
+        idx.insert(pos);
+        return false;
+    }
+};
