85beb762c356069ae0f95f53e6422e781dac462c
LIU Chengyang <464004340@qq.com>
2016-10-08 13:02:45 -0400

    Update

--- 330. Patching Array.cpp
+++ 330. Patching Array.cpp
@@ -19,3 +19,26 @@ public:
         
     }
 };
+
+
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        int patch = 0;
+        long long sum = 0;
+        int idx = 0;
+        while(sum < n){
+            if(idx < nums.size() && sum >= nums[idx]-1){
+                sum = sum + nums[idx];
+                idx++;
+               
+            }
+            else{
+                 patch++;
+                sum = sum+sum+1;
+            }
+            
+        }
+        return patch;
+    }
+};
