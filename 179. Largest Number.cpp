f93f2c9ad9a79f63c27013dda026440454a85fb5
LIU Chengyang <464004340@qq.com>
2016-10-09 14:14:53 -0400

    Update 179. Largest Number.cpp

--- 179. Largest Number.cpp
+++ 179. Largest Number.cpp
@@ -15,3 +15,28 @@ public:
         
     }
 };
+
+
+ bool great(string n1, string n2){
+        return n1+n2 > n2+n2;
+    }
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> ss;
+        for(auto n : nums) {
+            ss.push_back(to_string(n));
+        }
+        // cout << "Asd"<<endl;
+        sort(ss.begin(), ss.end(), great);
+        string rst;
+
+        for(auto s : ss) rst+=s;
+        int i = 0;
+        while(i < rst.size() && rst[i] == '0') i++;
+        return i == rst.size() ? "0":rst.substr(i);
+        
+        
+    }
+   
+};
