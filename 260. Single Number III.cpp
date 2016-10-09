853646ee2de5d39910c0ff8a0ffcbd3b85ddf79b
LIU Chengyang <464004340@qq.com>
2016-10-09 14:57:40 -0400

    Update 260. Single Number III.cpp

--- 260. Single Number III.cpp
+++ 260. Single Number III.cpp
@@ -16,3 +16,22 @@ public:
         
     }
 };
+
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        vector<int> rst(2,0);
+        int axorb = 0;
+        for(auto n:nums) axorb^=n;
+        int lastdigit = axorb&(~(axorb-1));
+        for(auto n : nums){
+            if(n&lastdigit){
+                rst[0] ^= n;
+            }
+            else{
+                rst[1] ^= n;
+            }
+        }
+        return rst;
+    }
+};
