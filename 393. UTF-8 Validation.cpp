0d2573d59eca0c3b015d9bf94b4da1b2a60ab213
LIU Chengyang <464004340@qq.com>
2016-10-09 17:53:27 -0400

    Create 393. UTF-8 Validation.cpp

---
+++ 393. UTF-8 Validation.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int cnt = 0;
+        for(auto  c:data){
+            if(cnt == 0){
+                if((c>>7) == 0b0) cnt = 0;
+                else if((c >> 5) == 0b110)cnt = 1;
+                else if((c>>4) ==0b1110)cnt = 2;
+                else if((c>>3) ==0b11110)cnt = 3;
+                else return false;
+            }
+            else{
+                if((c >>6) != 0b10)return false;
+                cnt--;
+            }
+        }
+       return cnt == 0;
+    }
+  
+};
