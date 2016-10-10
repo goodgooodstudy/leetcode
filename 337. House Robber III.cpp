95a9790ff485d40f85a096a272b58f16c651e1ff
LIU Chengyang <464004340@qq.com>
2016-10-10 11:12:14 -0400

    Update 337. House Robber III.cpp

--- 337. House Robber III.cpp
+++ 337. House Robber III.cpp
@@ -22,3 +22,34 @@ public:
         return max(l+r, ll+lr+rl+rr+root->val);
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int rob(TreeNode* root) {
+        vector<int> rst = helper(root);
+        return max(rst[0],rst[1]);
+        
+    }
+    vector<int> helper(TreeNode* root){
+        if(!root) return vector<int>(2,0);
+        int total = 0;
+        vector<int> left = helper(root->left);
+        vector<int> right = helper(root->right);
+        vector<int> res(2,0);
+        res[0] = max(left[0], left[1])+max(right[0], right[1]);
+        res[1] = root->val + left[0]+right[0];
+        return res;
+        
+        
+    }
+};
