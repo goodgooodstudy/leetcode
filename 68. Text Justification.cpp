90ec06dc7694450b006893cd467e3b78335a9eea
LIU Chengyang <464004340@qq.com>
2016-10-23 17:14:40 -0400

    Create 68. Text Justification.cpp

---
+++ 68. Text Justification.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+        vector<string> rst;
+        for(int i = 0, k, l; i < words.size(); i = i + k){
+            for(k =0, l = 0; i+k < words.size() && l + words[i+k].size() <= maxWidth-k; k++){
+                l = l+ words[i+k].size();
+            }
+            string tmp = words[i];
+            for(int j = 1; j < k; j++){
+                if(i+k == words.size()) tmp += " ";
+                else tmp += string((maxWidth-l)/(k-1) + (j <= (maxWidth-l)%(k-1)),' ');
+                tmp += words[i+j];
+            }
+            tmp += string(maxWidth-tmp.size(),' ');
+            rst.push_back(tmp);
+        }
+        return rst;
+        
+    }
+};
