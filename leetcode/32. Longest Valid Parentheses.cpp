cb4d1b651bf329e465f3a0e1925a80050a7768d0
LIU Chengyang <464004340@qq.com>
2016-11-03 21:39:04 -0400

    Update 32. Longest Valid Parentheses.cpp

--- leetcode/32. Longest Valid Parentheses.cpp
+++ leetcode/32. Longest Valid Parentheses.cpp
@@ -27,3 +27,28 @@ public:
         return max_len;
     }
 };
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        vector<int> dp(n,0);
+        int mx = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '(') dp[i] = 0;
+            else{
+                if(i-1 < 0) continue;
+                if(s[i-1] == '(') dp[i] = dp[i-2]+2;
+                else{
+                    int j = i-1 - dp[i-1]+1;
+                    if(j-1>=0 && s[j-1] == '('){
+                        dp[i] = dp[i-1]+2;
+                        if(j-2 >=0) dp[i]+=dp[j-2];
+                    }
+                }
+            }
+            mx = max(mx, dp[i]);
+        }
+        return mx;
+        
+    }
+};
