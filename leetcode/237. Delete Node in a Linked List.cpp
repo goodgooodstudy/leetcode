fd0d76a0e2e17fc742d75fc074dfd7c2aef00684
LIU Chengyang <464004340@qq.com>
2016-11-04 15:08:37 -0400

    Update 237. Delete Node in a Linked List.cpp

--- leetcode/237. Delete Node in a Linked List.cpp
+++ leetcode/237. Delete Node in a Linked List.cpp
@@ -23,3 +23,30 @@ public:
         
     }
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* cur = node;
+        ListNode* next = cur->next;
+        cur->val = next->val;
+        while(next->next){
+            cur = next;
+            next = next->next;
+            cur->val = next->val;
+        }
+        cur->next = nullptr;
+        delete(next);
+        
+        
+    }
+};
