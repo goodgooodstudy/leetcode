775072155291281a1e572c4b9bf31eef81a82fdf
LIU Chengyang <464004340@qq.com>
2016-11-04 15:00:01 -0400

    Update 12. Integer to Roman.cpp

--- leetcode/12. Integer to Roman.cpp
+++ leetcode/12. Integer to Roman.cpp
@@ -21,3 +21,21 @@ public:
         
     }
 };
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        string dict[13] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        int val[13] = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string rst;
+        for(int i = 0; i < 13; i++){
+            if(num >= val[i]){
+                for(int k = 0; k < num/val[i]; k++){
+                    rst += dict[i];
+                }
+                num = num % val[i];
+            }
+        }
+        return rst;
+    }
+};
