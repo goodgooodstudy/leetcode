1990ba803351c543e9cd73b670be9194dec2ab4e
qimoqiuguo <464004340@qq.com>
2016-11-03 23:55:08 -0400

    32

--- leetcode/32. Longest Valid Parentheses.cpp
+++ leetcode/32. Longest Valid Parentheses.cpp
@@ -1,9 +1,22 @@
+/**
+* @Author: lcy
+* @Date:   2016-09-03T12:50:54-04:00
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+
+
 class Solution {
 public:
     int longestValidParentheses(string s) {
         stack<int> stk;
         int max_len = 0;
-        int last = -1; 
+        int last = -1;
         for(int i = 0; i < s.size(); i++){
             if(s[i] == '('){
                 stk.push(i);
@@ -25,5 +38,30 @@ public:
             }
         }
         return max_len;
+    }
+};
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size();
+        vector<int> dp(n,0);
+        int mx = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '(') dp[i] = 0;
+            else{
+                if(i-1 < 0) continue;
+                if(s[i-1] == '(') dp[i] = dp[i-2]+2;
+                else{
+                    int j = i-1 - dp[i-1]+1;
+                    if(j-1>=0 && s[j-1] == '('){
+                        dp[i] = dp[i-1]+2;
+                        if(j-2 >=0) dp[i]+=dp[j-2];
+                    }
+                }
+            }
+            mx = max(mx, dp[i]);
+        }
+        return mx;
+
     }
 };
