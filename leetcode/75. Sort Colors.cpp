9f72a9dd6698da94c2ec0715f7ebd8d9ede2eb50
LIU Chengyang <464004340@qq.com>
2016-11-03 20:40:53 -0400

    Update 75. Sort Colors.cpp

--- leetcode/75. Sort Colors.cpp
+++ leetcode/75. Sort Colors.cpp
@@ -34,3 +34,18 @@ public:
         
     }
 };
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero = 0, second = nums.size()-1;
+        for(int i = 0; i <=second; i++){
+            if(nums[i] == 0) swap(nums[zero++], nums[i]);
+            else if(nums[i] == 2){
+                swap(nums[second--], nums[i]);
+                i--;
+            } 
+        }
+        
+        
+    }
+};
