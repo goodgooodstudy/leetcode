e7e618dcd0ec029946542de87d84ce1954970a06
LIU Chengyang <464004340@qq.com>
2016-11-04 15:12:32 -0400

    Update 191. Number of 1 Bits.cpp

--- leetcode/191. Number of 1 Bits.cpp
+++ leetcode/191. Number of 1 Bits.cpp
@@ -10,3 +10,16 @@ public:
         
     }
 };
+
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int total = 0;
+        while(n > 0){
+            if(n&0x1) total += 1;
+            n = n >> 1;
+        }
+        return total;
+    }
+};
