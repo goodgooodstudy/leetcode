4ea6825a1fe2cc44de8f2546fce6d3ecfdc45137
LIU Chengyang <464004340@qq.com>
2016-11-05 11:00:30 -0400

    Update 150. Evaluate Reverse Polish Notation.cpp

--- leetcode/150. Evaluate Reverse Polish Notation.cpp
+++ leetcode/150. Evaluate Reverse Polish Notation.cpp
@@ -28,3 +28,40 @@ public:
         else return p1*p2;
     } 
 };
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> sk;
+        unordered_set<string> op ={"+","-","*","/"};
+        for(string token : tokens){
+            if(op.count(token)){
+                int n2 = sk.top(); ///pay attention to / since n2 is divisor
+                sk.pop();
+                int n1 = sk.top();
+                sk.pop();
+                int res = compute(n1, n2, token);
+                sk.push(res);
+            }
+            else{
+                cout << stoi(token)<<endl;
+                sk.push(stoi(token));
+            }
+        }
+        return sk.top();
+        
+    }
+    int compute(int n1, int n2, string op){
+        if(op == "+"){
+            return n1 + n2;
+        }
+        else if(op == "-"){
+            return n1 - n2;
+        }
+        else if(op == "*"){
+            return n1*n2;
+        }
+        else{
+            return n1 / n2;
+        }
+    }
+};
