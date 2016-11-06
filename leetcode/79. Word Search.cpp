3d642d82a9e469efa864e0e84e31df359e245ff6
LIU Chengyang <464004340@qq.com>
2016-11-06 16:24:48 -0500

    Update 79. Word Search.cpp

--- leetcode/79. Word Search.cpp
+++ leetcode/79. Word Search.cpp
@@ -1,4 +1,35 @@
 class Solution {
+    int m;
+    int n;
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        if(board.size() == 0 || board[0].size() == 0) return false;
+        m = board.size();
+        n = board[0].size();
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(helper(board, i, j, word, 0)) return true;
+            }
+        }
+        return false;
+        
+    }
+    bool helper(vector<vector<char>>& board, int i, int j, string word, int idx){
+        if(board[i][j]!= word[idx]) return false;
+        if(idx == word.size()-1) return true; //!!! think clearly.....
+
+        if(board[i][j] == 0) return false;
+        char tmp = board[i][j];
+        board[i][j] = 0;
+        if(i-1>=0 &&helper(board, i-1, j, word, idx+1))return true;
+        if(i+1 < m && helper(board, i+1, j, word, idx+1)) return true;;
+        if(j-1>=0 && helper(board, i, j-1, word, idx+1)) return true;
+        if(j+1 < n && helper(board, i, j+1, word, idx+1)) return true;
+        board[i][j] = tmp;
+        return false;
+    }
+};
+class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
         if(board.size() == 0) return false;
