fbf9d864467c8cd00cc3281e811e9078ee0195ee
LIU Chengyang <464004340@qq.com>
2016-11-06 11:22:22 -0500

    Create 419. Battleships in a Board.cpp

---
+++ leetcode/419. Battleships in a Board.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int m = board.size(), n = board[0].size();
+        int cnt = 0;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(board[i][j] == 'X' && (i == 0||board[i-1][j] == '.') && (j == 0|| board[i][j-1] == '.')){
+                    cnt ++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
