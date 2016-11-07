375bd5be1f05402c93677b7f1ce7716ad1f91f59
LIU Chengyang <464004340@qq.com>
2016-11-07 09:34:18 -0500

    Update 205. Isomorphic Strings.cpp

--- leetcode/205. Isomorphic Strings.cpp
+++ leetcode/205. Isomorphic Strings.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> s2t;
+        unordered_map<char, char> t2s;
+        if(s.size()!=t.size()) return false;
+        for(int i = 0; i < (int)s.size(); i++){
+            if(s2t.count(s[i]) || t2s.count(t[i])){
+                if(s2t[s[i]]!=t[i] || t2s[t[i]]!=s[i]) return false;
+            }
+            else{
+                s2t[s[i]] = t[i];
+                t2s[t[i]] = s[i];
+            }
+        }
+        return true;
+    }
+};
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
         unordered_map<char, char> maps1;
         unordered_map<char, char> maps2;
 
