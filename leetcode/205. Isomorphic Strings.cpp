79504195ea4692d43a8a0ff5c7b9c9dd23a83032
LIU Chengyang <464004340@qq.com>
2016-11-07 09:34:06 -0500

    Update 205. Isomorphic Strings.cpp

--- leetcode/205. Isomorphic Strings.cpp
+++ leetcode/205. Isomorphic Strings.cpp
@@ -5,7 +5,7 @@ public:
         unordered_map<char, char> maps2;
 
         for(int i = 0; i<s.size(); i++){
-            if(maps1.count(s[i])||maps2.count(t[i])){
+            if(maps1.count(s[i])||maps2.count(t[i])){ // 两个都要！！！
                 if(t[i]!=maps1[s[i]]||s[i]!=maps2[t[i]]) return false;
             }
             else{
