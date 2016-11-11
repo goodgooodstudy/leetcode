fc0cda977a7d8096271f3f175840ac251b2320f1
LIU Chengyang <464004340@qq.com>
2016-11-11 18:17:52 -0500

    Update 99. Recover Binary Search Tree.cpp

--- leetcode/99. Recover Binary Search Tree.cpp
+++ leetcode/99. Recover Binary Search Tree.cpp
@@ -8,6 +8,37 @@
  * };
  */
 class Solution {
+public:
+    void recoverTree(TreeNode* root) {
+        TreeNode* pre = NULL, *first = NULL, *second = NULL;
+        findNode(root, pre, first, second);
+        int value = first->val;
+        first->val = second->val;
+        second->val = value;
+        
+    }
+    void findNode(TreeNode* root, TreeNode*& pre, TreeNode*& first, TreeNode*& second){
+        if(!root)return;
+        findNode(root->left, pre, first, second);
+        
+        if(pre != NULL && pre->val > root->val){
+            if(first == NULL) first = pre;
+            if(first!= NULL) second = root;
+        }
+        pre = root;
+        findNode(root->right, pre, first, second);
+    }
+};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
 private:
     TreeNode* first = NULL;
     TreeNode* second = NULL;
