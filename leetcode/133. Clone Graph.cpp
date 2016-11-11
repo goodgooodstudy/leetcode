11e2cc3a7c0fbeed333f729ce34458593f08f276
qimoqiuguo <464004340@qq.com>
2016-11-11 13:19:23 -0500

    ASD

--- leetcode/133. Clone Graph.cpp
+++ leetcode/133. Clone Graph.cpp
@@ -1,4 +1,17 @@
 /**
+* @Author: lcy
+* @Date:   2016-09-03T12:50:54-04:00
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+
+
+/**
  * Definition for undirected graph.
  * struct UndirectedGraphNode {
  *     int label;
@@ -22,5 +35,107 @@ public:
             cpnode->neighbors.push_back(clone(neighbor,cp));
         }
         return cpnode;
+    }
+};
+
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
+class Solution {
+private:
+    unordered_map<UndirectedGraphNode *, UndirectedGraphNode *> dict;
+public:
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        queue<UndirectedGraphNode*> q;
+        unordered_set<UndirectedGraphNode*> visited;
+        if(node) q.push(node);
+        else return NULL;
+        visited.insert(node);
+        dict[node] = new UndirectedGraphNode(node->label);
+        while(!q.empty()){
+            UndirectedGraphNode* cur = q.front();
+            q.pop();
+
+            for(auto neigh : cur->neighbors){
+                if(!dict.count(neigh)){
+                    dict[neigh] = new UndirectedGraphNode(neigh->label);
+                }
+                dict[cur]->neighbors.push_back(dict[neigh]);
+                if(!visited.count(neigh)){
+                    q.push(neigh);
+                    visited.insert(neigh); //有可能已经在queue里面但是没pop出来，如果再pop时候判断，就可能in queue了两次。可以用dictpanduan
+                }
+            }
+        }
+        return dict[node];
+
+    }
+};
+
+
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
+class Solution {
+private:
+    unordered_map<UndirectedGraphNode *, UndirectedGraphNode *> dict;
+public:
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        queue<UndirectedGraphNode*> q;
+        if(node) q.push(node);
+        else return NULL;
+        dict[node] = new UndirectedGraphNode(node->label);
+        while(!q.empty()){
+            UndirectedGraphNode* cur = q.front();
+            q.pop();
+
+            for(auto neigh : cur->neighbors){
+                if(!dict.count(neigh)){
+                    dict[neigh] = new UndirectedGraphNode(neigh->label);
+                    q.push(neigh);
+
+                }
+                dict[cur]->neighbors.push_back(dict[neigh]);
+
+            }
+        }
+        return dict[node];
+
+    }
+};
+/**
+ * Definition for undirected graph.
+ * struct UndirectedGraphNode {
+ *     int label;
+ *     vector<UndirectedGraphNode *> neighbors;
+ *     UndirectedGraphNode(int x) : label(x) {};
+ * };
+ */
+class Solution {
+private:
+    unordered_map<UndirectedGraphNode *, UndirectedGraphNode *> dict;
+public:
+    UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+        if(node == NULL) return NULL;
+        return clone(node);
+
+    }
+    UndirectedGraphNode* clone(UndirectedGraphNode* node){
+        if(dict.count(node)) return dict[node];
+        dict[node] = new UndirectedGraphNode(node->label);
+        for(auto neigh : node->neighbors){
+            dict[node]->neighbors.push_back(clone(neigh));
+        }
+        return dict[node];
     }
 };
--- leetcode/301. Remove Invalid Parentheses.cpp
+++ leetcode/301. Remove Invalid Parentheses.cpp
@@ -1,3 +1,61 @@
+/**
+* @Author: lcy
+* @Date:   2016-09-03T12:50:54-04:00
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+
+
+
+class Solution {
+public:
+    vector<string> removeInvalidParentheses(string s) {
+        int left = 0;
+        int right = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == '(') left++;
+            if(s[i] == ')'){
+                if(left > 0) left--;
+                else right++;
+            }
+        }
+        unordered_set<string> rst;
+        removehelper(s, 0, "", left, right, 0, rst);
+        return vector<string>(rst.begin(), rst.end());
+
+    }
+
+    void removehelper(string s, int idx, string cur, int remove_left, int remove_right, int pair, unordered_set<string>& rst){
+        if(idx == s.size()){
+            if(remove_left == 0 && remove_right == 0 && pair == 0){
+                rst.insert(cur);
+            }
+            return;
+        }
+            if(s[idx] == '('){
+                if(remove_left >0){
+                    removehelper(s, idx+1, cur,remove_left-1, remove_right, pair, rst);
+                }
+                removehelper(s, idx+1, cur+"(", remove_left, remove_right, pair+1, rst);
+            }
+            else if(s[idx] == ')'){
+                if(remove_right > 0){
+                    removehelper(s, idx+1, cur, remove_left, remove_right-1, pair, rst);
+                }
+                if(pair>0){
+                    removehelper(s, idx+1, cur+")", remove_left, remove_right, pair-1, rst);
+                }
+            }
+            else{
+                removehelper(s, idx+1, cur+s[idx], remove_left, remove_right, pair, rst);
+            }
+
+    }
+};
 class Solution {
 public:
     vector<string> removeInvalidParentheses(string s) {
@@ -5,7 +63,7 @@ public:
         queue<string> q;
         vector<string> res;
         bool found = false;
-        
+
         q.push(s);
         visited.insert(s);
         while(!q.empty()){
@@ -29,8 +87,8 @@ public:
             }
         }
         return res;
-        
-        
+
+
     }
     bool isValid(string s){
         int cnt = 0;
