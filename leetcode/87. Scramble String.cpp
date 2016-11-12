9ef992c50549429a605626373cc21492487ccf8a
LIU Chengyang <464004340@qq.com>
2016-11-11 23:42:38 -0500

    Update 87. Scramble String.cpp

--- leetcode/87. Scramble String.cpp
+++ leetcode/87. Scramble String.cpp
@@ -2,6 +2,23 @@ class Solution {
 public:
     bool isScramble(string s1, string s2) {
         if(s1 == s2) return true;
+        string cp1(s1);
+        string cp2(s2);
+        sort(cp1.begin(), cp1.end());
+        sort(cp2.begin(), cp2.end());
+        if(cp1 != cp2) return false;
+        int n = s1.size();
+        for(int i = 1; i < n; i++){
+            if(isScramble(s1.substr(0,i),s2.substr(0,i)) && isScramble(s1.substr(i), s2.substr(i))) return true;
+            if(isScramble(s1.substr(0,i), s2.substr(n-i))&&isScramble(s1.substr(i), s2.substr(0,n-i))) return true;
+        }
+        return false;
+    }
+};
+class Solution {
+public:
+    bool isScramble(string s1, string s2) {
+        if(s1 == s2) return true;
         if(s1.size()!=s2.size()) return false;
         int len = s1.size();
         int cout[128] = {0};
