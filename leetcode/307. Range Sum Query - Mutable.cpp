71581cbbfb727abd5ff103e83032eb0d4a072309
LIU Chengyang <464004340@qq.com>
2016-11-14 10:27:16 -0500

    Update 307. Range Sum Query - Mutable.cpp

--- leetcode/307. Range Sum Query - Mutable.cpp
+++ leetcode/307. Range Sum Query - Mutable.cpp
@@ -1,4 +1,52 @@
 class NumArray {
+private:
+    vector<int> BIT;
+    vector<int> n;
+public:
+    NumArray(vector<int> &nums) {
+        int n = nums.size();
+        if(n < 1) return;
+        this->BIT = vector<int>(n+1, 0);
+        this->n = vector<int>(n,0);
+        for(int i = 0; i < nums.size(); i++){
+            update(i, nums[i]);
+        }
+        
+    }
+
+    void update(int i, int val) {
+        int add = val-n[i];
+        n[i] = n[i]+add;
+        i++;
+        while(i < BIT.size()){
+            BIT[i] += add;
+            i = i + (i&(-i));
+        }
+    }
+    int getSum(int i){
+        i++;
+        int total = 0;
+        while(i >0){
+            total += BIT[i];
+            i = i - (i&(-i));
+        }
+        return total;
+    }
+    int sumRange(int i, int j) {
+        int s1 = 0, s2 = 0;
+        if(i-1>=0) s1 = getSum(i-1);
+        s2 = getSum(j);
+        return s2-s1;
+    }
+};
+
+
+// Your NumArray object will be instantiated and called as such:
+// NumArray numArray(nums);
+// numArray.sumRange(0, 1);
+// numArray.update(1, 10);
+// numArray.sumRange(1, 2);
+class NumArray {
     vector<int>sum;
 public:
     NumArray(vector<int> &nums):sum(nums.size(),0) {
