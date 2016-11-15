61b52fcbcf44fac0a008af2d2b8d3f968bf135d2
LIU Chengyang <464004340@qq.com>
2016-11-14 22:14:32 -0500

    Update 270. Closest Binary Search Tree Value.cpp

--- leetcode/270. Closest Binary Search Tree Value.cpp
+++ leetcode/270. Closest Binary Search Tree Value.cpp
@@ -9,6 +9,30 @@
  */
 class Solution {
 public:
+    int closestValue(TreeNode* root, double target) {  
+        int closest = root->val;
+        while(root){
+            if(abs(closest-target) >= abs(root->val - target))
+                closest = root->val;
+            root = target < root->val ? root->left:root->right;
+        }
+        
+        return closest;
+     
+     
+    }
+};
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
     int closestValue(TreeNode* root, double target) {
         if(root ==NULL) return 0;
         int tmp;
