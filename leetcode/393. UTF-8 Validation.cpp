8f603967240d931e61c8cb1c94a800704f2eebb6
LIU Chengyang <464004340@qq.com>
2016-12-11 20:15:20 -0500

    Update 393. UTF-8 Validation.cpp

--- leetcode/393. UTF-8 Validation.cpp
+++ leetcode/393. UTF-8 Validation.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     bool validUtf8(vector<int>& data) {
+        int idx = 0;
+        while( idx < data.size()){
+            int cnt = 0;
+            if(data[idx] >> 7 == 0) cnt = 0;
+            else if(data[idx] >> 5 == 0b110) cnt = 1;
+            else if(data[idx] >> 4 == 0b1110) cnt = 2;
+            else if(data[idx] >> 3 == 0b11110) cnt = 3;
+            else return false;
+            for(int i = 1; i <= cnt; i++){
+                if(data[idx+i] >> 6 != 0b10) return false;
+            }
+            idx = idx + cnt+1;
+        }
+        
+        return true;
+        
+    }
+};
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
         int cnt = 0;
         for(auto  c:data){
             if(cnt == 0){
