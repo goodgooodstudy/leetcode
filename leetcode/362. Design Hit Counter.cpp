859512aba37ee7ac6959e23e249f05f492e5e105
LIU Chengyang <464004340@qq.com>
2019-02-20 23:54:25 -0800

    Update 362. Design Hit Counter.cpp

--- leetcode/362. Design Hit Counter.cpp
+++ leetcode/362. Design Hit Counter.cpp
@@ -24,6 +24,50 @@ private:
     queue<int> q;
 };
 
+// What if the number of hits per second could be very large? Does your design scale?
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
+
+
+
+class HitCounter {
+private:
+    queue<pair<int,int>> pq;
+    int cnt;
+public:
+    /** Initialize your data structure here. */
+    HitCounter() {
+        cnt = 0;
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    void hit(int timestamp) {
+        while(!pq.empty() && timestamp - pq.front().first >= 300){
+            cnt -= pq.front().second;
+            pq.pop();
+        }
+        cnt ++;
+        if(!pq.empty() && timestamp == pq.back().first) pq.back().second+=1;
+        else pq.push(make_pair(timestamp, 1));
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    int getHits(int timestamp) {
+        while(!pq.empty() && timestamp - pq.front().first >= 300){
+            cnt -= pq.front().second;
+            pq.pop();
+        }
+        return cnt;
+    }
+};
+
 /**
  * Your HitCounter object will be instantiated and called as such:
  * HitCounter obj = new HitCounter();
