2109f3f2017cfcf9735d171dd92d2ee0b7cc87db
LIU Chengyang <464004340@qq.com>
2019-02-23 00:42:03 -0800

    Update 126. Word Ladder II.cpp

--- leetcode/126. Word Ladder II.cpp
+++ leetcode/126. Word Ladder II.cpp
@@ -67,3 +67,56 @@ public:
 
     }
 };
+
+
+
+class Solution {
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        queue<vector<string>>q;
+        unordered_set<string> word_dict(wordList.begin(), wordList.end());
+        unordered_set<string> visited;
+        vector<string> rst({beginWord});
+        q.push(rst);
+        bool found = false;
+        vector<vector<string>> routes;
+        while(!q.empty()){
+            unordered_set<string> cur_visited;
+            int n = q.size();
+            while(n-- > 0){
+                vector<string> cur = q.front();
+                q.pop();
+                string cur_word = cur.back();
+                vector<string> next_words = findNext(cur_word, word_dict, visited);
+                for(string s : next_words){
+                    vector<string> n(cur);
+                    n.push_back(s);
+                    cur_visited.insert(s);
+                    if(s == endWord) {
+                        found = true;
+                        routes.push_back(n);
+                    }
+                    q.push(n);
+                } 
+            }
+            for(string c : cur_visited) visited.insert(c);
+            if(found) return routes;
+        }
+        return routes;
+        
+        
+    }
+    vector<string> findNext(string word, unordered_set<string>& dict, unordered_set<string>& visited){
+        vector<string> res;
+        for(int i = 0; i < word.size(); i++){
+            char ori = word[i];
+            for(char c = 'a'; c <= 'z'; c++){
+                word[i] = c;
+                if(dict.count(word)&&!visited.count(word))
+                res.push_back(word);
+            }
+            word[i] = ori;
+        }
+        return res;
+    }
+};
